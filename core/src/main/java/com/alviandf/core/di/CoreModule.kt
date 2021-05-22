14f73cea2048a67cc4c57488e94b9025ba6406ab
Alvi <alviand03@gmail.com>
2021-05-22 23:29:27 +0700

    add security and leak canary

--- core/src/main/java/com/alviandf/core/di/CoreModule.kt
+++ core/src/main/java/com/alviandf/core/di/CoreModule.kt
@@ -8,6 +8,9 @@ import com.alviandf.core.data.source.remote.RemoteDataSource
 import com.alviandf.core.data.source.remote.network.ApiService
 import com.alviandf.core.domain.repository.ICatalogueAppRepository
 import com.alviandf.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,19 +22,26 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<MovieAndTvShowDatabase>().movieAndTvShowDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("alviandf".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             MovieAndTvShowDatabase::class.java, "movieAndTvShow.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
